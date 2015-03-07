ab47fb8a39d1822051f4ba1f23d666e91d96c990
LXGaming <alexrobthomson@outlook.com>
2015-03-08 11:55:09 +1300

    Small Changes

--- LXPlugin/src/main/java/io/github/lxgaming/lxplugin/LXPlugin.java
+++ LXPlugin/src/main/java/io/github/lxgaming/lxplugin/LXPlugin.java
@@ -4,7 +4,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
