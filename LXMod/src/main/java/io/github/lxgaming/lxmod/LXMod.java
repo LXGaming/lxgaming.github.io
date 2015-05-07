cab9aa180d189216e0346bb627a7ba0fcf1ffc70
Alex Thomson <alexrobthomson@outlook.com>
2015-05-07 20:06:39 +1200

    Added LXMod (Primary Mod Class)

---
+++ LXMod/src/main/java/io/github/lxgaming/lxmod/LXMod.java
@@ -0,0 +1,29 @@
+package io.github.lxgaming.lxmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_Name, version = Reference.VERSION)
+public class LXMod {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void Init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
