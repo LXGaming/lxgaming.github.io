4caf7805e22253e12e176b54396b19b505454edf
Alex Thomson <alexrobthomson@outlook.com>
2015-05-07 20:06:51 +1200

    Added Reference Class

---
+++ LXMod/src/main/java/io/github/lxgaming/lxmod/Reference.java
@@ -0,0 +1,7 @@
+package io.github.lxgaming.lxmod;
+
+public class Reference {
+	public static final String MOD_ID = "LXM";
+	public static final String MOD_Name = "LXMod";
+	public static final String VERSION = "1.0";
+}
