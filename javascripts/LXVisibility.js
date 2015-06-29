3c88965058195bdd0c13626753bd94da05762efe
Alex Thomson <alexrobthomson@outlook.com>
2015-06-29 21:11:39 +1200

    Updated 29/06/15
    Added "Main" Css Class

--- javascripts/LXVisibility.js
+++ javascripts/LXVisibility.js
@@ -2,6 +2,7 @@ function LXVisibility(){
 var NavigationBase = document.getElementById("NavigationBase").style;
 var Navigation = document.getElementById("Navigation").style;
 var myCanvas = document.getElementById("myCanvas").style;
+var Main = document.getElementById("Main").style;
 		
 if (NavigationBase.visibility=="hidden") NavigationBase.visibility = "visible";
 else NavigationBase.visibility = "hidden";
@@ -11,4 +12,7 @@ else Navigation.visibility = "hidden";
 
 if (myCanvas.visibility=="hidden") myCanvas.visibility = "visible";
 else myCanvas.visibility = "hidden";
+
+if (Main.visibility=="hidden") Main.visibility = "visible";
+else Main.visibility = "hidden";
 }
