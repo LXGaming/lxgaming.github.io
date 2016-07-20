5f0c5d3871bfcd53a92c17ac09a99f5553920a9f
Alex Thomson <alexrobthomson@outlook.com>
2016-07-20 17:56:52 +1200

    Removed Mesh

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -9,16 +9,6 @@ function Image() {
 	return;
 }
 
-function Mesh() {
-	var mesh = document.getElementById("mesh").style;
-	if (mesh.visibility == "visible") {
-		mesh.visibility = "hidden";
-	} else {
-		mesh.visibility = "visible";
-	}
-	return;
-}
-
 function ScrollDown() {
 	var body = $("body");
 	$("html, body").animate({ scrollTop: body.height()}, 2000);
