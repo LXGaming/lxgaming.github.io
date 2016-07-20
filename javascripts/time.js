efe43222f9e23e3ad7f72edc4dbe3f139c33c7ea
Alex Thomson <alexrobthomson@outlook.com>
2016-07-20 18:58:18 +1200

    More information

--- javascripts/time.js
+++ javascripts/time.js
@@ -8,6 +8,70 @@ function timeStart() {
 function update() {
   var time = new Date();
   document.getElementById("content").innerHTML = ""
-    + time.getUTCHours() + ":" + time.getUTCMinutes() + ":" + time.getUTCSeconds() + "<br>"
-    + time.getUTCDate() + "/" + (time.getUTCMonth() + 1) + "/" + time.getUTCFullYear();
+    + getFixedTime(time.getUTCHours()) + ":" + getFixedTime(time.getUTCMinutes()) + ":" + getFixedTime(time.getUTCSeconds()) + " " + getAMPM(time.getHours()) + "<br>"
+    + getDayName(time.getUTCDay()) + " " + time.getUTCDate() + " " + getMonthName(time.getUTCMonth()) + " " + time.getUTCFullYear();
+}
+
+function getFixedTime(time) {
+  if (time.toString().length == 1) {
+    return "0" + time;
+  }
+  return time;
+}
+
+function getAMPM(hour) {
+  if (hour > 12) {
+    return "PM"
+  }
+  return "AM";
+}
+
+function getDayName(day) {
+  if (day == 0) {
+    return "Error";
+  } else if (day == 1) {
+    return "Monday";
+  } else if (day == 2) {
+    return "Tuesday";
+  } else if (day == 3) {
+    return "Wednesday";
+  } else if (day == 4) {
+    return "Thursday";
+  } else if (day == 5) {
+    return "Friday";
+  } else if (day == 6) {
+    return "Saturday";
+  } else if (day == 7) {
+    return "Sunday";
+  }
+  return "Error";
+}
+
+function getMonthName(month) {
+  if (month == 0) {
+    return "January";
+  } else if (month == 1) {
+    return "February";
+  } else if (month == 2) {
+    return "March";
+  } else if (month == 3) {
+    return "April";
+  } else if (month == 4) {
+    return "May";
+  } else if (month == 5) {
+    return "June";
+  } else if (month == 6) {
+    return "July";
+  } else if (month == 7) {
+    return "August";
+  } else if (month == 8) {
+    return "September";
+  } else if (month == 9) {
+    return "October";
+  } else if (month == 10) {
+    return "November";
+  } else if (month == 11) {
+    return "December";
+  }
+  return "Error";
 }
