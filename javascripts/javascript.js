00d5835c18196cc78402ec939e3676ff333f3c2b
Alex Thomson <alexrobthomson@outlook.com>
2016-08-28 15:03:39 +1200

    Finally fixed Scroll animation

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -11,11 +11,11 @@ function Image() {
 
 function ScrollDown() {
 	var body = $("body");
-	$("html, body").animate({ scrollTop: body.height()}, 2000);
+	$("html, body").animate({ scrollTop: document.getElementById("content").offsetHeight}, 1500);
 	return;
 }
 
 function ScrollUp() {
-	$("html, body").animate({ scrollTop: 0}, 2000);
+	$("html, body").animate({ scrollTop: 0}, 1500);
 	return;
 }
