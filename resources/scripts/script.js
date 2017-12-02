936ab90d00829496dc20c2189e49b6e8cfedee7f
Alex Thomson <alexrobthomson@outlook.com>
2017-12-02 18:42:39 +1300

    Added Easter egg.
    Toggles website overlay when the Konami Code is entered.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -6,6 +6,18 @@ function onLoad() {
             scrollTop: $(".collection").offset().top
         }, 2000);
     });
+
+    $(window).konami();
+    $(window).on("konami", function() {
+        Materialize.toast("Konami Code!", 5000);
+        if ($(".visibility").css("visibility") === "visible") {
+            $(".visibility").css("visibility", "hidden");
+            $("body").css("overflow-y", "hidden");
+        } else {
+            $(".visibility").css("visibility", "visible");
+            $("body").css("overflow-y", "auto");
+        }
+    });
 }
 
 function onResize() {
