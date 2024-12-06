95ec66fb4028eb505e907414b96a818ea1e57abf
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2024-12-06 17:50:49 +1300

    Update configs

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+export default {
+  images: {
+    unoptimized: true
+  },
+  output: "export"
+} satisfies NextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,6 +10,4 @@ const config: Config = {
     extend: {}
   },
   plugins: []
-};
-
-export default config;
+} satisfies Config;
