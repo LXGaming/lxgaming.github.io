cfeb4229273076f50d5457371e3bfd80e86c4541
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2026-01-10 13:32:46 +1300

    Remove Web Analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout(props: RootLayoutProps) {
         <div className="container mx-auto p-3">
           <Providers>{props.children}</Providers>
         </div>
-        <script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "92a0fb32f04b46d4aa3605e7b9f96c24"}'></script>
       </body>
     </html>
   );
